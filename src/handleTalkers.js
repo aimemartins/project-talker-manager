89c9972c5505bb1d0994fb5b5a2378f033ff335d
AIMÊ <aimemartins@hotmail.com>
2023-02-06 18:08:20 -0300

    Requisito 2

--- src/handleTalkers.js
+++ src/handleTalkers.js
@@ -1,11 +1,11 @@
 const fs = require('fs').promises;
-// const path = require('path');
+const path = require('path');
 
-// const talkersPath = path.resolve(__dirname, '.', 'talker.json');
+const talkersPath = path.resolve(__dirname, '..', 'src', 'talker.json');
 
 async function getAllTalkers() {
   try {
-    const response = await fs.readFile('src/talker.json', 'utf8');
+    const response = await fs.readFile(talkersPath, 'utf8');
     // console.log(typeof response);
     const reString = JSON.parse(response);
     // console.log(typeof reString);
@@ -15,6 +15,20 @@ async function getAllTalkers() {
   }
 }
 
+async function getId(id) {
+  // console.log(typeof id);
+    const response = await fs.readFile(talkersPath, 'utf8');
+    const reString = JSON.parse(response);
+    const findId = reString.find((talker) => talker.id === id);
+    // console.log(findId);
+   if (!findId) {
+    throw new Error('Pessoa palestrante não encontrada');
+   } else {
+    return findId;
+   }
+}
+
 module.exports = {
   getAllTalkers,
+  getId,
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllTalkers } = require('./handleTalkers');
+const { getAllTalkers, getId } = require('./handleTalkers');
 
 const app = express();
 app.use(express.json());
@@ -7,16 +7,31 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.listen(PORT, () => {
+  console.log('Online');
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// Requisito 01
 
 app.get('/talker', async (_req, res) => {
   const talkers = await getAllTalkers();
   res.status(200).json(talkers);
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+// Requisito 02
+
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talkers = await getId(Number(id));
+    console.log(talkers);
+    res.status(200).json(talkers);
+  } catch (err) {
+    res.status(404).send({ message: err.message });
+  }
 });
